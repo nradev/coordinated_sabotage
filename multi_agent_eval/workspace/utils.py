63c7b4895c326e88b8c67a0f3f20fdcb5f252f75
lehaas <lennart.haas@outlook.com>
2025-09-25 16:47:47 +0200

    Remove content of workspace

--- multi_agent_eval/workspace/utils.py
+++
@@ -1,3 +0,0 @@
-def get_unique(input):
-    """Return the unique elements of the input list"""
-    raise NotImplementedError()
--- multi_agent_eval/workspace/validation.py
+++
@@ -1,3 +0,0 @@
-def validate_input(input):
-    """Validate that the input is valid for the digits function (NOT a bool as well)"""
-    raise NotImplementedError()
